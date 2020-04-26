97f642f23529ae5540e1c9be8180de1cf858c0b4
Jovidon <you@example.comjova_94@inbox.ru>
2020-04-26 00:18:40 -0700

    Second commit

---
+++ src/test/java/com/vytrack/pages/login_navigation/LoginPage.java
@@ -0,0 +1,87 @@
+package com.vytrack.pages.login_navigation;
+
+import com.vytrack.utilities.BasePage;
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.ConfigurationReader;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+    //public LoginPage(){PageFactory.initElements(Driver.getDriver(), this);}
+
+    @FindBy(id = "prependedInput")
+    @CacheLookup
+    public WebElement userNameElement;
+
+    @FindBy(id = "prependedInput2")
+    @CacheLookup
+    public WebElement passwordElement;
+
+    @FindBy(id="_submit")
+    public WebElement loginButtonElement;
+
+    @FindBy(className = "custom-checkbox__icon")
+    public WebElement rememberMeElement;
+
+    @FindBy(partialLinkText = "Forgot your password?")
+    public WebElement forgotPasswordElement;
+
+    @FindBy(tagName = "h2")
+    public WebElement titleElement;
+
+    @FindBy(css = "[class='alert alert-error'] > div")
+    public WebElement errorMessageElement;
+
+
+    public void login(String username, String password){
+        userNameElement.sendKeys(username);
+        passwordElement.sendKeys(password);
+        loginButtonElement.click();
+    }
+
+    public void login(){
+        String username = ConfigurationReader.getProperty("storemanagerusername");
+        String password = ConfigurationReader.getProperty("storemanagerpassword");
+        userNameElement.sendKeys(username);
+        passwordElement.sendKeys(password);
+        loginButtonElement.click();
+    }
+
+    public String getErrorMessage(){
+        return errorMessageElement.getText();
+    }
+
+    public void clickRememberMe(){
+        BrowserUtils.waitForClickablility(rememberMeElement, Integer.valueOf(ConfigurationReader.getProperty("SHORT_WAIT")));
+        if(!rememberMeElement.isSelected()){
+            rememberMeElement.click();
+        }
+    }
+
+
+
+
+    public void login(String role){
+        String username="";
+        String password="";
+
+        if(role.equalsIgnoreCase("driver")){
+            username=ConfigurationReader.getProperty("driverusername");
+            password=ConfigurationReader.getProperty("password");
+        }else if(role.equalsIgnoreCase("storemanager")){
+            username=ConfigurationReader.getProperty("storemanagerusername");
+            password=ConfigurationReader.getProperty("storemanagerpassword");
+        }else if(role.equalsIgnoreCase("salesmanager")){
+            username=ConfigurationReader.getProperty("salesmanagerusername");
+            password=ConfigurationReader.getProperty("salesmanagerpassword");
+        }
+
+        userNameElement.sendKeys(username);
+        passwordElement.sendKeys(password);
+        loginButtonElement.click();
+
+
+    }
+
+}
